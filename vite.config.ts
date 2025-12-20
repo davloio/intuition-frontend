ceac152d8c3ac4050a8367916fba9486beadf215
Loïs Kauffungen <lois.kauffungen@gmail.com>
2025-12-20 10:55:07 +0100

    update allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    host: true
+    port: 2000,
+    host: true,
+    allowedHosts: [process.env.VITE_HOST || 'localhost']
   }
 })
